59b997f64c00b5d4283c76d6c3b96171d2838c69
Sonu Sah <sonusah2363@gmail.com>
2025-10-05 03:59:34 +0530

    made changes in db.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import express from "express"
 import cors from "cors"
 import itemRoutes from "./routes/item.routes.js"
-import {connectDB, createDatabase} from "./config/db.js"
-import Item from "./models/item.model.js";
+import {connectDB, seedItems, createDatabase} from "./config/db.js"
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,17 +19,6 @@ await createDatabase();
 await connectDB();
 
 // Seed data function
-async function seedItems() {
-  const count = await Item.count();
-  if (count === 0) {
-    await Item.bulkCreate([
-      { name: "Apple", description: "Fresh apples", stock_quantity: 50, price: 1.5 },
-      { name: "Banana", description: "Ripe bananas", stock_quantity: 30, price: 1.0 },
-      { name: "Orange", description: "Juicy oranges", stock_quantity: 20, price: 2.0 }
-    ]);
-    console.log("✅ Seed data inserted");
-  }
-}
 
 await seedItems();
 
--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2/promise";
+import Item from "../models/item.model.js";
 
 const DB_NAME = "inventory_db"
 const DB_USER = "root"
@@ -38,6 +39,18 @@ export async function connectDB() {
     console.log("✅ All models synchronized!");
   } catch (error) {
     console.error("❌ Database connection failed:", error);
+  }
+}
+
+export async function seedItems() {
+  const count = await Item.count();
+  if (count === 0) {
+    await Item.bulkCreate([
+      { name: "Apple", description: "Fresh apples", stock_quantity: 50, price: 1.5 },
+      { name: "Banana", description: "Ripe bananas", stock_quantity: 30, price: 1.0 },
+      { name: "Orange", description: "Juicy oranges", stock_quantity: 20, price: 2.0 }
+    ]);
+    console.log("✅ Seed data inserted");
   }
 }
 
